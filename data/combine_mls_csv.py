9f3a54060a6a507ecc91b2e6bf54e24bb79b05b2
Jeff Lin <42981468+jleifnf@users.noreply.github.com>
2020-01-03 16:47:30 -0500

    update combine_mls_csv.py

--- data/combine_mls_csv.py
+++ data/combine_mls_csv.py
@@ -9,19 +9,21 @@ from multiprocessing import Pool
 # ('Single Family Residence', 'Condominium', 'Townhouse',)
 # *** all area for the 12 days ago from 10/29/19
 
-def readListings(fNo):
-    mlsDB = pd.read_csv(os.path.join(projPath, 'Full (%s).csv' % fNo), delimiter=',', quoting=csv.QUOTE_MINIMAL,
+def readListings(fName):
+    mlsDB = pd.read_csv(fName, delimiter=',', quoting=csv.QUOTE_MINIMAL,
                         header=0, low_memory=False)
     return mlsDB
 
+
 rootPath = os.getcwd()
-projPath = os.path.join(rootPath,'mls_csv')
-
-xlsFiles = list(filter(re.compile('Full').match, sorted(os.listdir(projPath))))
-pool = Pool(int(max([min([len(xlsFiles) / 10, 4]), 1])))
-mlsDB = pool.map(readListings, range(0, len(xlsFiles)))
-mlsDB = pd.concat(mlsDB, ignore_index=True)
-mlsDB = mlsDB.drop_duplicates(subset='Matrix_Unique_ID', keep='first')
-mlsDB.to_csv(os.path.join(projPath, 'Full (0).csv'), sep=',', header=mlsDB.columns, index=False)
-pool.close()
-
+for folder in ['la_mls_csv', 'sd_mls_csv']:
+    projPath = os.path.join(rootPath, folder)
+    df = []
+    xlsFiles = list(filter(re.compile('Full').match, sorted(os.listdir(projPath))))
+    if len(xlsFiles) > 0:
+        for file in xlsFiles:
+            filename = os.path.join(projPath, file)
+            df.append(readListings(filename))
+        mlsDB = pd.concat(df, ignore_index=True)
+        mlsDB = mlsDB.drop_duplicates(subset='Matrix_Unique_ID', keep='first')
+        mlsDB.to_csv(os.path.join(projPath, 'Full (0).csv'), sep=',', header=mlsDB.columns, index=False)
