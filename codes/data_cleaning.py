ca9249172fa67a41aabce87314b4e93832108dd0
Jeff Lin <42981468+jleifnf@users.noreply.github.com>
2020-01-06 11:26:26 -0500

    edit codes/data_cleaning.py

--- codes/data_cleaning.py
+++ codes/data_cleaning.py
@@ -10,13 +10,16 @@ def compile_csv(data_path=None):
         proj_path = os.getcwd().replace('/codes', '')
         data_path = os.path.join(proj_path, 'data')
     csv_files = list(filter(re.compile('Full').match, sorted(os.listdir(data_path))))
+
+    cols_tokeep = ['CountyOrParish', 'OnMarketDate', 'CloseDate',
+                   'CumulativeDaysOnMarket','ListPrice', 'ClosePrice']
+
     dfs = []
     for file in csv_files:
         filename = os.path.join(data_path, file)
         df = pd.read_csv(filename, delimiter=',', quoting=csv.QUOTE_MINIMAL,
-                         header=0, low_memory=False)
+                         usecols=cols_tokeep, header=0, low_memory=False)
         dfs.append(df)
     mls_df = pd.concat(dfs, ignore_index=True)
-    return mls_df
 
-# mls_df = compile_csv()
+    return mls_df
