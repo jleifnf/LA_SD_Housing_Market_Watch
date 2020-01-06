f5dceb0f93d5f9d7406c072142f20f04612f75ae
Jeff Lin <42981468+jleifnf@users.noreply.github.com>
2020-01-06 10:03:15 -0500

    add data_cleaning.py

--- data/combine_mls_csv.py
+++ data/combine_mls_csv.py
@@ -1,29 +1,35 @@
 """ Look through downloaded MLS data and combine into a single CSV file. """
-import csv, pandas as pd, os, re
-from multiprocessing import Pool
+import csv
+import os
+import pandas as pd
+import re
+import numpy as np
 
 
-# past 575 days from 10/27/19 for:
-# Temple City, West Covina, Rosemead/S-San Gabriel, Arcadia [605,651,661,669]
-# 15 days from 10/27/19 from Fontana, Alhambra, Diamond Bar, El Monte [264,601,616,619]
-# ('Single Family Residence', 'Condominium', 'Townhouse',)
-# *** all area for the 12 days ago from 10/29/19
-
-def readListings(fName):
+def read_listings(fName):
     mlsDB = pd.read_csv(fName, delimiter=',', quoting=csv.QUOTE_MINIMAL,
                         header=0, low_memory=False)
+    #
+    list_agent_cols = list(filter(re.compile('ListAgent').match, mlsDB.columns))
+    buyer_agent_cols = list(filter(re.compile('BuyerAgent').match, mlsDB.columns))
+    text_cols = ['PublicRemarks', 'PrivateRemarks',
+                 'SyndicationRemarks', 'ShowingInstructions']
+    mlsDB[list_agent_cols + buyer_agent_cols + text_cols] = ''
+    #
     return mlsDB
 
 
 rootPath = os.getcwd()
-for folder in ['la_mls_csv', 'sd_mls_csv']:
-    projPath = os.path.join(rootPath, folder)
-    df = []
-    xlsFiles = list(filter(re.compile('Full').match, sorted(os.listdir(projPath))))
-    if len(xlsFiles) > 0:
-        for file in xlsFiles:
-            filename = os.path.join(projPath, file)
-            df.append(readListings(filename))
-        mlsDB = pd.concat(df, ignore_index=True)
-        mlsDB = mlsDB.drop_duplicates(subset='Matrix_Unique_ID', keep='first')
-        mlsDB.to_csv(os.path.join(projPath, 'Full (0).csv'), sep=',', header=mlsDB.columns, index=False)
+projPath = os.path.join(rootPath, 'mls_csv')
+df = []
+xlsFiles = list(filter(re.compile('Full').match, sorted(os.listdir(projPath))))
+if len(xlsFiles) > 0:
+    for file in xlsFiles:
+        filename = os.path.join(projPath, file)
+        df.append(read_listings(filename))
+    mlsDB = pd.concat(df, ignore_index=True)
+    mlsDB = mlsDB.drop_duplicates(subset='Matrix_Unique_ID', keep='first')
+    segments = np.array_split(mlsDB, round(mlsDB.shape[0] / 5e3) + 1, axis=0)
+    for i, df in enumerate(segments):
+        df.to_csv(os.path.join(projPath, f'Full_{i}.csv'), sep=',',
+                  quoting=csv.QUOTE_MINIMAL, header=mlsDB.columns, index=False)
